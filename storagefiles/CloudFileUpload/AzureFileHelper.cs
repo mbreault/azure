36600a771f6844c24d3d47085cd94b445825815c
mbreault@gmail.com <mbreault@gmail.com>
2019-03-14 20:52:14 -0400

    Azure Storage File upload example

---
+++ storagefiles/CloudFileUpload/AzureFileHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
+using System.IO;
+
+namespace CloudFileUpload
+{
+    class AzureFileHelper
+    {
+        public static void Upload(string filePath)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string storageKey = configuration.GetConnectionString("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageKey);
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = fileClient.GetShareReference("oreilly");
+            CloudFileDirectory root = share.GetRootDirectoryReference();
+
+            FileInfo file = new FileInfo(filePath);
+            CloudFile cloudFile = root.GetFileReference(file.Name);
+
+            var exists = cloudFile.ExistsAsync();
+
+            if (!exists.Result)
+            {
+                Console.WriteLine("{0} uploading",file.Name);
+                using (FileStream fs = file.OpenRead())
+                {
+                    var result = cloudFile.UploadFromStreamAsync(fs);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} exists", file.Name);
+            }
+        }
+    }
+}
---
+++ storagefiles/CloudFileUpload/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+
+namespace CloudFileUpload
+{
+    class Program
+    {
+        private const string APPINSIGHTS_INSTRUMENTATIONKEY = "REDACTED";
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY", APPINSIGHTS_INSTRUMENTATIONKEY);
+            TelemetryConfiguration telemetryConfiguration = TelemetryConfiguration.Active;
+            telemetryConfiguration.TelemetryInitializers.Add(new OperationCorrelationTelemetryInitializer());
+            telemetryConfiguration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+
+            var telemetryClient = new TelemetryClient();
+
+            // Set session data:
+            telemetryClient.Context.User.Id = Environment.UserName;
+            telemetryClient.Context.Session.Id = Guid.NewGuid().ToString();
+            telemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+
+            string filePath = @"C:\temp\test.txt";
+            AzureFileHelper.Upload(filePath);
+
+            // before exit, flush the remaining data
+            telemetryClient.Flush();
+
+            // Allow time for flushing:
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
