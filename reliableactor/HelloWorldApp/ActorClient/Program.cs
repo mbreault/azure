6d314300e7ee0e518c39c61a918e46c0647c8a93
mbreault@gmail.com <mbreault@gmail.com>
2019-04-05 11:22:10 -0400

    Service Fabric Actor Pattern Example

---
+++ reliableactor/HelloWorldApp/ActorClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWorld.Interfaces;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelemetryConfiguration configuration = TelemetryConfiguration.Active;
+
+            configuration.InstrumentationKey = "66fdd030-0383-4e52-9981-c69f985f292c";
+            configuration.TelemetryInitializers.Add(new OperationCorrelationTelemetryInitializer());
+
+            var telemetryClient = new TelemetryClient();
+            using (InitializeDependencyTracking(configuration))
+            {
+                // run app...
+
+                telemetryClient.TrackTrace("Hello World!");
+
+                IHelloWorld actor = ActorProxy.Create<IHelloWorld>(ActorId.CreateRandom(), new Uri("fabric:/HelloWorldApp/HelloWorldActorService"));
+                Task<string> retval = actor.GetHelloWorldAsync();
+                Console.Write(retval.Result);
+                Console.ReadLine();
+            }
+
+            // before exit, flush the remaining data
+            telemetryClient.Flush();
+
+            // flush is not blocking so wait a bit
+            Task.Delay(5000).Wait();
+
+        }
+
+        static DependencyTrackingTelemetryModule InitializeDependencyTracking(TelemetryConfiguration configuration)
+        {
+            var module = new DependencyTrackingTelemetryModule();
+
+            // prevent Correlation Id to be sent to certain endpoints. You may add other domains as needed.
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.chinacloudapi.cn");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.cloudapi.de");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.usgovcloudapi.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("localhost");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("127.0.0.1");
+
+            // enable known dependency tracking, note that in future versions, we will extend this list. 
+            // please check default settings in https://github.com/Microsoft/ApplicationInsights-dotnet-server/blob/develop/Src/DependencyCollector/DependencyCollector/ApplicationInsights.config.install.xdt
+
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.ServiceBus");
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.EventHubs");
+
+            // initialize the module
+            module.Initialize(configuration);
+
+            return module;
+        }
+    }
+}
---
+++ reliableactor/HelloWorldApp/HelloWorld/HelloWorld.cs
@@ -0,0 +1,39 @@
+﻿using HelloWorld.Interfaces;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.ServiceFabric;
+using Microsoft.ApplicationInsights.ServiceFabric.Module;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class HelloWorld : Actor, IHelloWorld
+    {
+        public HelloWorld(ActorService actorService, ActorId actorId)
+            : base(actorService, actorId)
+        {
+            var config = TelemetryConfiguration.Active;
+            config.TelemetryInitializers.Add(FabricTelemetryInitializerExtension.CreateFabricTelemetryInitializer(this.ActorService.Context));
+
+            var requestTrackingModule = new ServiceRemotingRequestTrackingTelemetryModule();
+            var dependencyTrackingModule = new ServiceRemotingDependencyTrackingTelemetryModule();
+            requestTrackingModule.Initialize(config);
+            dependencyTrackingModule.Initialize(config);
+        }
+
+        public Task<string> GetHelloWorldAsync()
+        { 
+            TelemetryClient telemetryClient = new TelemetryClient();
+            telemetryClient.InstrumentationKey = "66fdd030-0383-4e52-9981-c69f985f292c";
+            telemetryClient.TrackEvent("Hello from my reliable actor Event");
+            telemetryClient.Flush();
+            return Task.FromResult("Hello from my reliable actor!");
+        }
+
+    }
+}
