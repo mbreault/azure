f0ebebae879883e7b5d636c2831fae22fd64a227
mbreault@gmail.com <mbreault@gmail.com>
2019-04-08 17:23:51 -0400

    Created dotnet47 example

---
+++ storagefiles/dotnet47/AzureBlobDotNet47/AzureBlobDotNet47/AzureFileHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace AzureBlobDotNet47
+{
+    class AzureFileHelper
+    {
+
+        public static void Upload(string filePath)
+        {
+            string storageKey = System.Configuration.ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageKey);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            var containers = blobClient.ListContainers();
+
+            // Get a reference to the file share we created previously.
+            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference("reports");
+
+            FileInfo file = new FileInfo(filePath);
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(file.Name);
+            cloudBlockBlob.UploadFromFile(filePath);
+           
+        }
+    }
+}
---
+++ storagefiles/dotnet47/AzureBlobDotNet47/AzureBlobDotNet47/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobDotNet47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\temp\test.txt";
+            AzureFileHelper.Upload(filePath);
+        }
+    }
+}
