d015fd453dbcc641641c79b4c017f68ef147834a
mbreault@gmail.com <mbreault@gmail.com>
2019-04-22 16:32:54 -0400

    ldap example

---
+++ ldap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+
+namespace ldap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> userNameList = new List<string>();
+            string username = @"REDACTED";
+            string password = @"REDACTED";
+
+            using (PrincipalContext context = new PrincipalContext(ContextType.Domain, "REDACTED", username, password))
+            {
+                using (UserPrincipal user = new UserPrincipal(context))
+                {
+                    using (PrincipalSearcher searcher = new PrincipalSearcher(user))
+                    {
+                        foreach (System.DirectoryServices.AccountManagement.UserPrincipal result in searcher.FindAll())
+                        {
+                            Console.WriteLine(result.Name);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
