553e776a9026f68289409c4533d48d32e77d9780
Mike Breault <mikeb@mcrtechnologies.com>
2019-06-18 14:38:16 -0400

    SF remoting example

---
+++ servicefabric/ServiceRemoting/HelloWorldClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorldService;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWorldService helloWorldClient = ServiceProxy.Create<IHelloWorldService>(new Uri("fabric:/ServiceRemoting/HelloWorldService"));
+
+            string message = helloWorldClient.HelloWorldAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ servicefabric/ServiceRemoting/HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace HelloWorldService
+{
+    public interface IHelloWorldService : IService
+    {
+        Task<string> HelloWorldAsync();
+    }
+}
